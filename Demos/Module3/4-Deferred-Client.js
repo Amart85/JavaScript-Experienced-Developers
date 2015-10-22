8942b2be1c326aee38e039c026b845f552cf1f1d
Christopher Harrison <chrhar@microsoft.com>
2015-10-22 16:44:35 -0700

    Mod 2 demos

---
+++ Demos/Module3/4-Deferred-Client.js
@@ -0,0 +1,7 @@
+﻿function Client() {
+	var worker = new Worker('./4-Deferred.js');
+
+	worker.onmessage = function (e) {
+
+	};
+}
---
+++ Demos/Module3/4-Deferred-Worker.js
@@ -0,0 +1,13 @@
+﻿// Sample worker script
+
+self.onmessage = function (e) {
+	sleep(e.data.milliseconds);
+	self.postMessage({ message: 'Slept for ' + e.data.milliseconds + 'milliseconds' });
+}
+
+function sleep(miliseconds) {
+	var startingTime = new Date().getTime();
+	var stopTime = startingTime + miliseconds;
+
+	while (stopTime >= new Date().getTime()) { }
+}
